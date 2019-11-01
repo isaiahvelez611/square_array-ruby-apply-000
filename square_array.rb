7f4bb5288ff22c78aea1a5b4f222c7a718db1b8b
Isaiah Velez <isaiah.velez611@gmail.com>
2019-11-01 23:45:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,31 @@
+#puts "#"#{name}: #{name.length}"
+#array.each do |num| print i*2, "\n" end
+#{ |n| puts "Current number is: #{n}" }
+   # .each{|num| print i*2, "\n"}
+    # Math.sqrt(num) 
+
+
 def square_array(array)
-  # your code here
-end
+  array.each{|arr| print Math.sqrt(arr)}
+ end
+ 
+ # new_array = []
+#   array.each {|i| array << i ** 2 }
+#       new_array
+  #
+  
+
+
+  
+# def square_array(array)
+# array.each do {|arr|
+#     if (Math.sqrt(arr)).is_a? Integer
+#       arr << Math.sqrt(arr)
+#     else
+#       new_ar << (num * num)
+#     end
+#   }
+#   puts new_arr
+# #array.each do |arr|
+# #puts Math.sqrt(arr)"
+# end
