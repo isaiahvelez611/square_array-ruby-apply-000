10e24604c171033ec87ba78f3be95e937635ad0e
Isaiah Velez <isaiah.velez611@gmail.com>
2019-11-02 01:16:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -12,32 +12,3 @@ def square_array(array)
 end
  
  
-# def square_array(array)
-#   arr = []
-#   array.each { |i| arr << i ** 2 }
-#   arr
-# end
-
-# my_arr = [1, 2]
-# p square_array(my_arr)
- 
- # new_array = []
-#   array.each {|i| array << i ** 2 }
-#       new_array
-  #
-  
-
-
-  
-# def square_array(array)
-# array.each do {|arr|
-#     if (Math.sqrt(arr)).is_a? Integer
-#       arr << Math.sqrt(arr)
-#     else
-#       new_ar << (num * num)
-#     end
-#   }
-#   puts new_arr
-# #array.each do |arr|
-# #puts Math.sqrt(arr)"
-# end
